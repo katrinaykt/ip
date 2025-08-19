7edc411828da662ae9fefa630e1559f5a6a6831f
katrinaykt <katrinayeo@hotmail.com>
2025-08-19 23:54:39 +0800

    Added echo commands

--- src/main/java/Khat.java
+++ src/main/java/Khat.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Khat {
     public static void main(String[] args) {
 
+        String command;
+        Scanner userInputScanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Khat.\nWhat can I do for you?\n");
+        command = userInputScanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = userInputScanner.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
