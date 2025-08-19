00202044848a18b1b6abac85a0fd1a4fc1335a5b
katrinaykt <katrinayeo@hotmail.com>
2025-08-19 23:28:30 +0800

    Renamed chatbot and added greeting

---
+++ src/main/java/Khat.java
@@ -0,0 +1,7 @@
+public class Khat {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! I'm Khat.\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
