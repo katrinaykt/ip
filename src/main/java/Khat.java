4584f02ee8b2e8478c842c3c0396aa9aad6deb1c
katrinaykt <katrinayeo@hotmail.com>
2025-08-20 00:09:46 +0800

    Added storing ability and display

--- src/main/java/Khat.java
+++ src/main/java/Khat.java
@@ -4,13 +4,26 @@ public class Khat {
     public static void main(String[] args) {
 
         String command;
+        int commandCounter = 0;
+        String[] commandList = new String[100];
         Scanner userInputScanner = new Scanner(System.in);
 
-        System.out.println("Hello! I'm Khat.\nWhat can I do for you?\n");
+        System.out.println("Hello! I'm Khat.\nWhat can I do for you?");
         command = userInputScanner.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(command);
-            command = userInputScanner.nextLine();
+            if (command.equals("list")) {
+                for (int i = 0; i < commandCounter; i++) {
+                    String currCommand = commandList[i];
+                    System.out.println(i + 1 + ". " + currCommand);
+                }
+                command = userInputScanner.nextLine();
+            } else {
+                System.out.println("added: " + command);
+                commandList[commandCounter] = command;
+                commandCounter++;
+                command = userInputScanner.nextLine();
+            }
+
         }
 
         System.out.println("Bye. Hope to see you again soon!");
